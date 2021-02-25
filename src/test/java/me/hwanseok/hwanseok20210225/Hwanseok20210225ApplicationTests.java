20958527552c94d4b4c956e4b21c6a9862516122
hwanseok.dev <hwanseok.dev@gmail.com>
2021-02-25 14:06:19 +0900

    add hello controller

---
+++ src/test/java/me/hwanseok/hwanseok20210225/Hwanseok20210225ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hwanseok.hwanseok20210225;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hwanseok20210225ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
